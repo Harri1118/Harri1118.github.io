043206de79b4d44c509b0831e73d459308068070
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2024-12-11 15:13:25 -0500

    Fixed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,31 @@
-import React, { useEffect } from 'react';
-import { Typography, Box } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Typography, Box, IconButton } from '@mui/material';
+import { ArrowBack, ArrowForward } from '@mui/icons-material';
 import AOS from 'aos';
+import '../css/Header.css';
 import 'aos/dist/aos.css';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+const images = [
+  'https://media.licdn.com/dms/image/v2/D4E35AQHdsxZsWWeGNw/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1724352473197?e=1734544800&v=beta&t=U7vcq3gXd64hB2utTVi5wav9QTu_rdagXRcFgJ6piQU',
+  'https://i.ytimg.com/vi/i3p42HhTjvw/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDfPlbs1cWl6zOzdzgQfeXgP9MdKA',
+  'https://i.ytimg.com/vi/mtOD19C7J48/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLD5cyiU8QVwf-HMwgOgERM6JluUnw'
+];
 
 function Header() {
+  const [currentImage, setCurrentImage] = useState(0);
+
   useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
+
+  const handleNext = () => {
+    setCurrentImage((prev) => (prev + 1) % images.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentImage((prev) => (prev - 1 + images.length) % images.length);
+  };
 
   return (
     <Box
@@ -18,17 +37,56 @@ function Header() {
       <Typography variant="h1" component="h2" data-aos="fade-in" fontFamily={"Ring of Kerry, sans-serif"}>
         Welcome
       </Typography>
-      <Box
-        component="img"
-        sx={{
-        height: 233,
-        width: 350,
-        maxHeight: { xs: 233, md: 167 },
-        maxWidth: { xs: 350, md: 250 },
-      }}
-        alt="The house from the offer."
-        src="https://media.licdn.com/dms/image/v2/D4E35AQHdsxZsWWeGNw/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1724352473197?e=1734544800&v=beta&t=U7vcq3gXd64hB2utTVi5wav9QTu_rdagXRcFgJ6piQU"
-      />
+      <Box sx={{ position: 'relative', display: 'inline-block', width: '70%', height: '400px', overflow: 'hidden' }}>
+        <TransitionGroup>
+          <CSSTransition
+            key={currentImage}
+            timeout={500}
+            classNames="slide"
+          >
+            <Box
+              component="img"
+              sx={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+              }}
+              alt="The house from the offer."
+              src={images[currentImage]}
+            />
+          </CSSTransition>
+        </TransitionGroup>
+        <IconButton
+          onClick={handlePrev}
+          sx={{ position: 'absolute', top: '50%', left: 0, transform: 'translateY(-50%)' }}
+        >
+          <ArrowBack />
+        </IconButton>
+        <IconButton
+          onClick={handleNext}
+          sx={{ position: 'absolute', top: '50%', right: 0, transform: 'translateY(-50%)' }}
+        >
+          <ArrowForward />
+        </IconButton>
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+        {images.map((_, index) => (
+          <Box
+            key={index}
+            sx={{
+              height: 10,
+              width: 10,
+              borderRadius: '50%',
+              backgroundColor: currentImage === index ? 'primary.main' : 'grey.400',
+              mx: 0.5,
+            }}
+          />
+        ))}
+      </Box>
       <Typography variant="h2" component="h2" data-aos="fade-in">
         Goal
       </Typography>
