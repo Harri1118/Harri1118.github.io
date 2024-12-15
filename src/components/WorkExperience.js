aac943e005032930031a19a21830b04844938a07
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2024-12-14 22:09:03 -0500

    10pm

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -335,8 +335,307 @@
     </Timeline>
  * 
  */
+// import { useEffect, useRef, useState } from 'react';
+// import { Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineDot, TimelineOppositeContent } from '@mui/lab';
+// import { Paper, Typography, Box, CardActions } from '@mui/material';
+// import WorkIcon from '@mui/icons-material/Work';
+// import SchoolIcon from '@mui/icons-material/School';
+// import StarIcon from '@mui/icons-material/Star';
+// import AOS from 'aos';
+// import '../css/WorkExperience.css';
+// import objects from '../objectClassifications/objects';
+// import NestedModal from '../elements/NestedModal';
 
-    import { useEffect, useRef, useState } from 'react';
+// let count = 0;
+
+// function chooseBackGround(bg) {
+//   if (bg === "work") {
+//     return 'rgb(33, 150, 243)';
+//   } else if (bg === "education") {
+//     return 'rgb(233, 30, 99)';
+//   }
+//   return '';
+// }
+
+// function chooseColorType(c) {
+//   if (c === "work") return "primary";
+//   else if (c === "education") return "secondary";
+//   return "";
+// }
+
+// function chooseIcon(ic) {
+//   if (ic === "work") return (<WorkIcon />);
+//   else if (ic === "education") return (<SchoolIcon />);
+// }
+
+// function WorkExperience() {
+//   const [jobs, setJobs] = useState([]);
+//   const elementsRef = useRef([]);
+
+//   useEffect(() => {
+//     setJobs(objects.Jobs);
+//     const observer = new IntersectionObserver(
+//       (entries) => {
+//         entries.forEach((entry) => {
+//           if (entry.isIntersecting) {
+//             entry.target.classList.add('fade-in');
+//           }
+//         });
+//       },
+//       { threshold: 0.1 }
+//     );
+
+//     elementsRef.current.forEach((element) => {
+//       if (element) {
+//         observer.observe(element);
+//       }
+//     });
+
+//     return () => {
+//       elementsRef.current.forEach((element) => {
+//         if (element) {
+//           observer.unobserve(element);
+//         }
+//       });
+//     };
+//   }, []);
+
+//   useEffect(() => {
+//     AOS.init({ duration: 1000 });
+//   }, []);
+
+//   function displayTimeLineItem(item, index) {
+//     const isOdd = index % 2 === 1;  // Use index to determine if it's odd or even
+//     return (
+//       <TimelineItem key={item.jobTitle}>
+//         {isOdd ? (
+//           <>
+//             <TimelineOppositeContent style={{ display: 'flex', justifyContent: 'center' }}>
+//               <Paper elevation={3} data-aos="fade-up" style={{ padding: '6px 16px', background: chooseBackGround(item.timelineType), color: '#fff' }}>
+//                 <Typography variant="h6" component="h1">
+//                   {item.jobTitle} (opposite content)
+//                 </Typography>
+//                 <Typography>{item.location}</Typography>
+//                 <Typography>{item.description}</Typography>
+//                 <CardActions sx={{ justifyContent: 'center' }}>
+//                   <NestedModal size="small" title={item.jobTitle} description={item.description} skills={item.skills} />
+//                 </CardActions>
+//               </Paper>
+//             </TimelineOppositeContent>
+//             <TimelineSeparator>
+//               <TimelineDot color={chooseColorType(item.timelineType)}>
+//                 {chooseIcon(item.timelineType)}
+//               </TimelineDot>
+//               <TimelineConnector />
+//             </TimelineSeparator>
+//             <TimelineContent style={{ display: 'flex', justifyContent: 'center' }}>
+//               <Typography variant="body2" color="textSecondary" data-aos="fade-in">
+//                 {item.startDate} - {item.endDate}
+//               </Typography>
+//             </TimelineContent>
+//           </>
+//         ) : (
+//           <>
+//             <TimelineOppositeContent style={{ display: 'flex', justifyContent: 'center' }}>
+//               <Typography variant="body2" color="textSecondary" data-aos="fade-in">
+//                 {item.startDate} - {item.endDate}
+//               </Typography>
+//             </TimelineOppositeContent>
+//             <TimelineSeparator>
+//               <TimelineDot color={chooseColorType(item.timelineType)}>
+//                 {chooseIcon(item.timelineType)}
+//               </TimelineDot>
+//               <TimelineConnector />
+//             </TimelineSeparator>
+//             <TimelineContent style={{ display: 'flex', justifyContent: 'center' }}>
+//               <Paper elevation={3} data-aos="fade-up" style={{ padding: '6px 16px', background: chooseBackGround(item.timelineType), color: '#fff' }}>
+//                 <Typography variant="h6" component="h1">
+//                   {item.jobTitle} (content)
+//                 </Typography>
+//                 <Typography>{item.location}</Typography>
+//                 <Typography>{item.description}</Typography>
+//                 <CardActions sx={{ justifyContent: 'center' }}>
+//                   <NestedModal size="small" title={item.jobTitle} description={item.description} skills={item.skills} />
+//                 </CardActions>
+//               </Paper>
+//             </TimelineContent>
+//           </>
+//         )}
+//       </TimelineItem>
+//     );
+//   }
+  
+//   return (
+//     <Timeline align="alternate" data-aos="fade-up">
+//       {jobs.map((job, index) => displayTimeLineItem(job, index))}
+//       <TimelineItem>
+//         <TimelineSeparator>
+//           <TimelineDot color="success">
+//             <StarIcon />
+//           </TimelineDot>
+//         </TimelineSeparator>
+//         <TimelineContent>
+//           <Box display="flex" justifyContent="center" />
+//         </TimelineContent>
+//       </TimelineItem>
+//     </Timeline>
+//   );
+// }
+
+// export default WorkExperience;
+
+// import { useEffect, useRef, useState } from 'react';
+// import { Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineDot, TimelineOppositeContent } from '@mui/lab';
+// import { Paper, Typography, Box, CardActions } from '@mui/material';
+// import WorkIcon from '@mui/icons-material/Work';
+// import SchoolIcon from '@mui/icons-material/School';
+// import StarIcon from '@mui/icons-material/Star';
+// import AOS from 'aos';
+// import '../css/WorkExperience.css';
+// import objects from '../objectClassifications/objects';
+// import NestedModal from '../elements/NestedModal';
+
+// let count = 0;
+
+// function chooseBackGround(bg) {
+//   if (bg === "work") {
+//     return 'rgb(33, 150, 243)';
+//   } else if (bg === "education") {
+//     return 'rgb(233, 30, 99)';
+//   }
+//   return '';
+// }
+
+// function chooseColorType(c) {
+//   if (c === "work") return "primary";
+//   else if (c === "education") return "secondary";
+//   return "";
+// }
+
+// function chooseIcon(ic) {
+//   if (ic === "work") return (<WorkIcon />);
+//   else if (ic === "education") return (<SchoolIcon />);
+// }
+
+// function WorkExperience() {
+//   const [jobs, setJobs] = useState([]);
+//   const elementsRef = useRef([]);
+
+//   useEffect(() => {
+//     setJobs(objects.Jobs);
+//     const observer = new IntersectionObserver(
+//       (entries) => {
+//         entries.forEach((entry) => {
+//           if (entry.isIntersecting) {
+//             entry.target.classList.add('fade-in');
+//           }
+//         });
+//       },
+//       { threshold: 0.1 }
+//     );
+
+//     elementsRef.current.forEach((element) => {
+//       if (element) {
+//         observer.observe(element);
+//       }
+//     });
+
+//     return () => {
+//       elementsRef.current.forEach((element) => {
+//         if (element) {
+//           observer.unobserve(element);
+//         }
+//       });
+//     };
+//   }, []);
+
+//   useEffect(() => {
+//     AOS.init({ duration: 1000 });
+//   }, []);
+
+//   function displayTimeLineItem(item, index) {
+//     const isOdd = index % 2 === 1;  // Use index to determine if it's odd or even
+//     return (
+//       <TimelineItem key={item.jobTitle}>
+//         {isOdd ? (
+//           <>
+//             <TimelineOppositeContent style={{ display: 'flex', justifyContent: 'center' }}>
+//               <Paper elevation={3} data-aos="fade-up" style={{ padding: '6px 16px', background: chooseBackGround(item.timelineType), color: '#fff' }}>
+//                 <Typography variant="h6" component="h1">
+//                   {item.jobTitle} (opposite content)
+//                 </Typography>
+//                 <Typography>{item.location}</Typography>
+//                 <Typography>{item.description}</Typography>
+//                 <CardActions sx={{ justifyContent: 'center' }}>
+//                   <NestedModal size="small" title={item.jobTitle} description={item.description} skills={item.skills} />
+//                 </CardActions>
+//               </Paper>
+//             </TimelineOppositeContent>
+//             <TimelineSeparator>
+//               <TimelineDot color={chooseColorType(item.timelineType)}>
+//                 {chooseIcon(item.timelineType)}
+//               </TimelineDot>
+//               <TimelineConnector />
+//             </TimelineSeparator>
+//             <TimelineContent style={{ display: 'flex', justifyContent: 'center' }}>
+//               <Typography variant="body2" color="textSecondary" data-aos="fade-in">
+//                 {item.startDate} - {item.endDate}
+//               </Typography>
+//             </TimelineContent>
+//           </>
+//         ) : (
+//           <>
+//             <TimelineOppositeContent style={{ display: 'flex', justifyContent: 'center' }}>
+//               <Typography variant="body2" color="textSecondary" data-aos="fade-in">
+//                 {item.startDate} - {item.endDate}
+//               </Typography>
+//             </TimelineOppositeContent>
+//             <TimelineSeparator>
+//               <TimelineDot color={chooseColorType(item.timelineType)}>
+//                 {chooseIcon(item.timelineType)}
+//               </TimelineDot>
+//               <TimelineConnector />
+//             </TimelineSeparator>
+//             <TimelineContent style={{ display: 'flex', justifyContent: 'center' }}>
+//               <Paper elevation={3} data-aos="fade-up" style={{ padding: '6px 16px', background: chooseBackGround(item.timelineType), color: '#fff' }}>
+//                 <Typography variant="h6" component="h1">
+//                   {item.jobTitle} (content)
+//                 </Typography>
+//                 <Typography>{item.location}</Typography>
+//                 <Typography>{item.description}</Typography>
+//                 <CardActions sx={{ justifyContent: 'center' }}>
+//                   <NestedModal size="small" title={item.jobTitle} description={item.description} skills={item.skills} />
+//                 </CardActions>
+//               </Paper>
+//             </TimelineContent>
+//           </>
+//         )}
+//       </TimelineItem>
+//     );
+//   }
+  
+//   return (
+//     <Box display="flex" justifyContent="center" width="100%" sx={{ margin: '0 auto', padding: '20px' }}>
+//       <Timeline align="alternate" data-aos="fade-up">
+//         {jobs.map((job, index) => displayTimeLineItem(job, index))}
+//         <TimelineItem>
+//           <TimelineSeparator>
+//             <TimelineDot color="success">
+//               <StarIcon />
+//             </TimelineDot>
+//           </TimelineSeparator>
+//           <TimelineContent>
+//             <Box display="flex" justifyContent="center" />
+//           </TimelineContent>
+//         </TimelineItem>
+//       </Timeline>
+//     </Box>
+//   );
+// }
+
+// export default WorkExperience;
+
+import { useEffect, useRef, useState } from 'react';
 import { Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineDot, TimelineOppositeContent } from '@mui/lab';
 import { Paper, Typography, Box, CardActions } from '@mui/material';
 import WorkIcon from '@mui/icons-material/Work';
@@ -346,6 +645,7 @@ import AOS from 'aos';
 import '../css/WorkExperience.css';
 import objects from '../objectClassifications/objects';
 import NestedModal from '../elements/NestedModal';
+
 let count = 0;
 
 function chooseBackGround(bg) {
@@ -404,50 +704,89 @@ function WorkExperience() {
     AOS.init({ duration: 1000 });
   }, []);
 
-  function displayTimeLineItem(item, c) {
+  function displayTimeLineItem(item, index) {
+    const isOdd = index % 2 === 1;  // Use index to determine if it's odd or even
     return (
-      <TimelineItem>
-        <TimelineOppositeContent style={{ display: 'flex', justifyContent: 'center' }}>
-          <Paper elevation={3} data-aos="fade-up" style={{ padding: '6px 16px', background: chooseBackGround(item.timelineType), color: '#fff' }}>
-            <Typography variant="h6" component="h1">
-              {item.jobTitle} (opposite content)
-            </Typography>
-            <Typography>{item.location}</Typography>
-            <Typography>{item.description}</Typography>
-            <CardActions sx={{ justifyContent: 'center' }}>
-              <NestedModal size="small" title={item.jobTitle} description={item.description} skills={item.skills} />
-            </CardActions>
-          </Paper>
-        </TimelineOppositeContent>
-        <TimelineSeparator>
-          <TimelineDot color={chooseColorType(item.timelineType)}>
-            {chooseIcon(item.timelineType)}
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent style={{ display: 'flex', justifyContent: 'center' }}>
-          <Typography variant="body2" color="textSecondary" data-aos="fade-in">
-            {item.startDate} - {item.endDate}
-          </Typography>
-        </TimelineContent>
+      <TimelineItem key={item.jobTitle}>
+        {isOdd ? (
+          <>
+            <TimelineOppositeContent style={{ display: 'flex', justifyContent: 'center' }}>
+              <Paper elevation={3} data-aos="fade-up" style={{ padding: '6px 16px', background: chooseBackGround(item.timelineType), color: '#fff' }}>
+                <Typography variant="h6" component="h1">
+                  {item.jobTitle} (opposite content)
+                </Typography>
+                <Typography>{item.location}</Typography>
+                <Typography>{item.description}</Typography>
+                <CardActions sx={{ justifyContent: 'center' }}>
+                  <NestedModal size="small" title={item.jobTitle} description={item.description} skills={item.skills} />
+                </CardActions>
+              </Paper>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color={chooseColorType(item.timelineType)}>
+                {chooseIcon(item.timelineType)}
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent style={{ display: 'flex', justifyContent: 'center' }}>
+              <Typography variant="body2" color="textSecondary" data-aos="fade-in">
+                {item.startDate} - {item.endDate}
+              </Typography>
+            </TimelineContent>
+          </>
+        ) : (
+          <>
+            <TimelineOppositeContent style={{ display: 'flex', justifyContent: 'center' }}>
+              <Typography variant="body2" color="textSecondary" data-aos="fade-in">
+                {item.startDate} - {item.endDate}
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color={chooseColorType(item.timelineType)}>
+                {chooseIcon(item.timelineType)}
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent style={{ display: 'flex', justifyContent: 'center' }}>
+              <Paper elevation={3} data-aos="fade-up" style={{ padding: '6px 16px', background: chooseBackGround(item.timelineType), color: '#fff' }}>
+                <Typography variant="h6" component="h1">
+                  {item.jobTitle} (content)
+                </Typography>
+                <Typography>{item.location}</Typography>
+                <Typography>{item.description}</Typography>
+                <CardActions sx={{ justifyContent: 'center' }}>
+                  <NestedModal size="small" title={item.jobTitle} description={item.description} skills={item.skills} />
+                </CardActions>
+              </Paper>
+            </TimelineContent>
+          </>
+        )}
       </TimelineItem>
     );
   }
-
+  
   return (
-    <Timeline align="alternate" data-aos="fade-up">
-      {jobs.map((job) => displayTimeLineItem(job, count++))}
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot color="success">
-            <StarIcon />
-          </TimelineDot>
-        </TimelineSeparator>
-        <TimelineContent>
-          <Box display="flex" justifyContent="center" />
-        </TimelineContent>
-      </TimelineItem>
-    </Timeline>
+    // <Box 
+    //   display="flex" 
+    //   justifyContent="center" 
+    //   width="100%" 
+    //   sx={{ margin: '0 auto', padding: '20px' }}
+    // 
+        <Timeline  data-aos="fade-up">
+          {jobs.map((job, index) => displayTimeLineItem(job, index))}
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot color="success">
+                <StarIcon />
+              </TimelineDot>
+            </TimelineSeparator>
+            <TimelineContent>
+              <Box display="flex" justifyContent="center" />
+            </TimelineContent>
+          </TimelineItem>
+        </Timeline>
+      
+    // </Box>
   );
 }
 
