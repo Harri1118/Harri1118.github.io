85c64c4b0cf8497746cf6a52db07364a5063dc95
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2025-01-26 12:06:57 -0500

    Fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,90 +16,23 @@ function Footer() {
     AOS.init({ duration: 1000 });
   }, []);
   return (
-    // <div id="footer" >
-    //   <Box paddingTop="100px"/>
-    //   <Typography  variant="h3" component="h3" data-aos="fade-up" sx={{fontFamily: 'Bruno Ace SC, serif', fontStyle: 'normal', color: '#7BAFD4', marginBottom: '20px'}}>
-    //     Contact Me
-    //   </Typography>
+    <div id="footer" >
+      <Box paddingTop="100px"/>
+      <Typography  variant="h3" component="h3" data-aos="fade-up" sx={{fontFamily: 'Bruno Ace SC, serif', fontStyle: 'normal', color: '#7BAFD4', marginBottom: '20px'}}>
+        Contact Me
+      </Typography>
   
-    //   <Box 
+      <Box 
         
-    //     sx={{ 
-    //       display: 'flex', 
-    //       justifyContent: 'center', 
-    //       alignItems: 'center', 
-    //       paddingTop: "5%", 
-    //       gap: 10  // Add space between icons
-    //     }}
-    //   >
-    //     <Link 
-    //       href="https://github.com/Harri1118" 
-    //       target="_blank" 
-    //       rel="noopener noreferrer" 
-    //       color="#7BAFD4"
-    //       sx={{
-    //         transition: "transform 0.3s ease, box-shadow 0.3s ease",
-    //         "&:hover": {
-    //           transform: "scale(1.05)",
-    //           boxShadow: "0 4px 20px rgba(0, 0, 0, 0.2)",
-    //         }
-    //       }}
-    //     >
-    //       <GitHubIcon fontSize="large"/>
-    //     </Link>
-  
-    //     <Link 
-    //       href="https://www.linkedin.com/in/harrison-mckenna-908104224/" 
-    //       target="_blank" 
-    //       rel="noopener noreferrer" 
-    //       color="#7BAFD4"
-    //       sx={{
-    //         transition: "transform 0.3s ease, box-shadow 0.3s ease",
-    //         "&:hover": {
-    //           transform: "scale(1.05)",
-    //           boxShadow: "0 4px 20px rgba(0, 0, 0, 0.2)",
-    //         }
-    //       }}
-    //     >
-    //       <LinkedInIcon fontSize="large" />
-    //     </Link>
-  
-    //     <Link 
-    //       href="https://www.linkedin.com/in/harrison-mckenna-908104224/" 
-    //       target="_blank" 
-    //       rel="noopener noreferrer" 
-    //       color="#7BAFD4"
-    //       sx={{
-    //         transition: "transform 0.3s ease, box-shadow 0.3s ease",
-    //         "&:hover": {
-    //           transform: "scale(1.05)",
-    //           boxShadow: "0 4px 20px rgba(0, 0, 0, 0.2)",
-    //         }
-    //       }}
-    //     >
-    //       <YouTubeIcon fontSize="large" />
-    //     </Link>
-    //     <Link 
-    //       href="https://www.linkedin.com/in/harrison-mckenna-908104224/" 
-    //       target="_blank" 
-    //       rel="noopener noreferrer" 
-    //       color="#7BAFD4"
-    //       sx={{
-    //         transition: "transform 0.3s ease, box-shadow 0.3s ease",
-    //         "&:hover": {
-    //           transform: "scale(1.05)",
-    //           boxShadow: "0 4px 20px rgba(0, 0, 0, 0.2)",
-    //         }
-    //       }}
-    //     >
-    //       <EmailIcon fontSize="large" />
-    //     </Link>
-    //   </Box>
-    //   <Box paddingTop="200px"/>
-    // </div>
-<div>
-<Grid2 container spacing={2} justifyContent="center" padding={2}>
-    <Link 
+        sx={{ 
+          display: 'flex', 
+          justifyContent: 'center', 
+          alignItems: 'center', 
+          paddingTop: "5%", 
+          gap: 10  // Add space between icons
+        }}
+      >
+        <Link 
           href="https://github.com/Harri1118" 
           target="_blank" 
           rel="noopener noreferrer" 
@@ -114,8 +47,57 @@ function Footer() {
         >
           <GitHubIcon fontSize="large"/>
         </Link>
-</Grid2>
-</div>
+  
+        <Link 
+          href="https://www.linkedin.com/in/harrison-mckenna-908104224/" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          color="#7BAFD4"
+          sx={{
+            transition: "transform 0.3s ease, box-shadow 0.3s ease",
+            "&:hover": {
+              transform: "scale(1.05)",
+              boxShadow: "0 4px 20px rgba(0, 0, 0, 0.2)",
+            }
+          }}
+        >
+          <LinkedInIcon fontSize="large" />
+        </Link>
+  
+        <Link 
+          href="https://www.linkedin.com/in/harrison-mckenna-908104224/" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          color="#7BAFD4"
+          sx={{
+            transition: "transform 0.3s ease, box-shadow 0.3s ease",
+            "&:hover": {
+              transform: "scale(1.05)",
+              boxShadow: "0 4px 20px rgba(0, 0, 0, 0.2)",
+            }
+          }}
+        >
+          <YouTubeIcon fontSize="large" />
+        </Link>
+        <Link 
+          href="https://www.linkedin.com/in/harrison-mckenna-908104224/" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          color="#7BAFD4"
+          sx={{
+            transition: "transform 0.3s ease, box-shadow 0.3s ease",
+            "&:hover": {
+              transform: "scale(1.05)",
+              boxShadow: "0 4px 20px rgba(0, 0, 0, 0.2)",
+            }
+          }}
+        >
+          <EmailIcon fontSize="large" />
+        </Link>
+      </Box>
+      <Box paddingTop="200px"/>
+    </div>
+
   );
   
 }
