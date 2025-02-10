77800d737a90ee4606ac329f486ab46c1f5dca48
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2025-02-09 19:43:33 -0500

    Modified objexts file and split it up. (Hopefully) fixed the font issue. Added more details to skills

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,12 +2,12 @@ import MediaCard from '../elements/MediaCard';
 import React, { useEffect, useState } from 'react';
 import AOS from 'aos';
 import { Grid2 } from '@mui/material';
-import objects from '../objectClassifications/objects.json'
+import projectsOrigin from '../objectClassifications/Projects.json'
 
 function Projects() {
     const[projects, setProjects] = useState([])
     useEffect(() => {
-        setProjects(objects.Projects)
+        setProjects(projectsOrigin.Projects)
         AOS.init({ duration: 1000 });
     }, []);
 
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,137 +1,7 @@
-// import { Box, FormControl, InputLabel, MenuItem, Select, Typography} from '@mui/material';
-// import React, { useEffect, useState } from 'react';
-// import AOS from 'aos';
-// import objects from '../objectClassifications/objects.json'
-// import SkillsIcons from '../elements/SkillsIcons';
-
-
-// function Skills() {
-//     const filterOptions = ['Languages', 'Frameworks', 'Tools']
-//     const [filter, setFilter] = useState('all');
-
-//     const handleChange = (event) => {
-//       setFilter(event.target.value);
-//     };
-
-//     function getFilterTitle(s){
-//         if(s == "plang")
-//             return filterOptions[0]
-//         else if(s == "frwork"){
-//             return filterOptions[1]
-//         }
-//         else if(s == "tool")
-//             return filterOptions[2]
-//         else{
-//             return "NULL"
-//         }
-//     }
-
-//     useEffect(() => {
-//         AOS.init({ duration: 1000 });
-//     }, []);
-
-
-//     return (
-//     <div
-//     data-aos="fade-up"
-//     style={{ 
-//         display: 'flex', 
-//         flexDirection: 'column',  // Change flexDirection to column to stack items
-//         alignItems: 'center',     // Center align horizontally
-//         //justifyContent: 'center'  // Center align vertically
-//     }}
-// >   
-// <FormControl sx={{ m: 1, width: "10%" }} size="small">
-//       <InputLabel id="demo-select-small-label">Filter by...</InputLabel>
-//       <Select
-//         labelId="demo-select-small-label"
-//         id="demo-select-small"
-//         value={filter}
-//         onChange={handleChange}
-//       >
-//         <MenuItem value={"all"}>All</MenuItem>
-//         <MenuItem value={"plang"}>Languages</MenuItem>
-//         <MenuItem value={"frwork"}>Frameworks</MenuItem>
-//         <MenuItem value={"tool"}>Tools</MenuItem>
-//       </Select>
-//     </FormControl>
-//     {
-//         (filter == 'all') ? (
-//         <div>
-//         <Typography  
-//         variant="h6" 
-//         component="h6" 
-//         sx={{
-//             fontFamily: 'Bruno Ace SC, serif', 
-//             fontStyle: 'normal', 
-//             color: '#7BAFD4'
-//         }}
-//         > 
-//         Languages
-//     </Typography>
-//         <Box paddingTop={5} >
-//                 <SkillsIcons skills={objects.Skills} filter="plang"/>
-//         </Box>
-//     <Typography  
-//         variant="h6" 
-//         component="h6" 
-//         sx={{
-//             fontFamily: 'Bruno Ace SC, serif', 
-//             fontStyle: 'normal', 
-//             color: '#7BAFD4'
-//         }}
-//         > 
-//         Frameworks
-//     </Typography>
-//         <Box paddingTop={5}>
-//                 <SkillsIcons skills={objects.Skills} filter="frwork"/>
-//         </Box>
-//     <Typography  
-//         variant="h6" 
-//         component="h6" 
-        
-//         sx={{
-//             fontFamily: 'Bruno Ace SC, serif', 
-//             fontStyle: 'normal', 
-//             color: '#7BAFD4'
-//         }}
-//         > 
-//         Tools
-//     </Typography>
-//         <Box paddingTop={5}>
-//                 <SkillsIcons skills={objects.Skills} filter="tool"/>
-//         </Box>
-//     </div>
-//         ):(
-//         <div>
-//     <Typography  
-//         variant="h6" 
-//         component="h6" 
-//         sx={{
-//             fontFamily: 'Bruno Ace SC, serif', 
-//             fontStyle: 'normal', 
-//             color: '#7BAFD4'
-//         }}
-//         > 
-//         {getFilterTitle(filter)}
-//     </Typography>
-//         <Box paddingTop={5}>
-//                 <SkillsIcons skills={objects.Skills} animate="true" filter={filter}/>
-//         </Box>
-//         </div>
-//         )
-//     } 
-// </div>
-
-//     );
-// }
-
-// export default Skills;
-
 import { Box, FormControl, InputLabel, MenuItem, Select, Typography, useMediaQuery } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import AOS from 'aos';
-import objects from '../objectClassifications/objects.json'
+import SkillsObjects from '../objectClassifications/Skills.json'
 import SkillsIcons from '../elements/SkillsIcons';
 
 function Skills() {
@@ -216,7 +86,7 @@ function Skills() {
                             Languages
                         </Typography>
                         <Box paddingTop={5}>
-                            <SkillsIcons skills={objects.Skills} filter="plang" />
+                            <SkillsIcons skills={SkillsObjects.Skills} filter="plang" />
                         </Box>
                         <Typography
                             variant="h6"
@@ -230,7 +100,7 @@ function Skills() {
                             Frameworks
                         </Typography>
                         <Box paddingTop={5}>
-                            <SkillsIcons skills={objects.Skills} filter="frwork" />
+                            <SkillsIcons skills={SkillsObjects.Skills} filter="frwork" />
                         </Box>
                         <Typography
                             variant="h6"
@@ -244,7 +114,7 @@ function Skills() {
                             Tools
                         </Typography>
                         <Box paddingTop={5}>
-                            <SkillsIcons skills={objects.Skills} filter="tool" />
+                            <SkillsIcons skills={SkillsObjects.Skills} filter="tool" />
                         </Box>
                     </div>
                 ) : (
@@ -261,7 +131,7 @@ function Skills() {
                             {getFilterTitle(filter)}
                         </Typography>
                         <Box paddingTop={5}>
-                            <SkillsIcons skills={objects.Skills} animate="true" filter={filter} />
+                            <SkillsIcons skills={SkillsObjects.Skills} animate="true" filter={filter} />
                         </Box>
                     </div>
                 )
